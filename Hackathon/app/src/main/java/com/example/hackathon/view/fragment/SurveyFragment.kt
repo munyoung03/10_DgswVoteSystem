ad9fe5002805d6970331c9f2236c8628e14796cc
LMY <lmun768@naver.com>
2020-09-08 15:47:27 +0900

    [UPDATE] Survey

--- Hackathon/app/src/main/java/com/example/hackathon/view/fragment/SurveyFragment.kt
+++ Hackathon/app/src/main/java/com/example/hackathon/view/fragment/SurveyFragment.kt
@@ -1,17 +1,14 @@
 package com.example.hackathon.view.fragment
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import android.graphics.Color
 import com.example.hackathon.R
 import com.example.hackathon.base.BaseFragment
 import com.example.hackathon.databinding.FragmentSurveyBinding
 import com.example.hackathon.viewmodel.SurveyViewModel
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import kotlinx.android.synthetic.main.fragment_survey.*
 
 class SurveyFragment : BaseFragment<FragmentSurveyBinding, SurveyViewModel>() {
 
@@ -21,6 +18,46 @@ class SurveyFragment : BaseFragment<FragmentSurveyBinding, SurveyViewModel>() {
         get() = R.layout.fragment_survey
 
     override fun init() {
+        val entries  = ArrayList<BarEntry>()
+        val color = mutableListOf<Int>(
+            0xFF6083FF.toInt()
+        )
+
+        entries.add(BarEntry(1f, 1f))
+        entries.add(BarEntry(1.2f, 2f))
+        entries.add(BarEntry(1.4f, 3f))
+        entries.add(BarEntry(1.6f, 4f))
+        entries.add(BarEntry(1.8f, 5f))
+
+
+        val dataSet = BarDataSet(entries, "")
+
+        val data = BarData(dataSet)
+        data.setValueTextSize(11f)
+        data.setValueTextColor(Color.WHITE)
+        data.barWidth = 0.1f
+        dataSet.setDrawValues(false)
+        bar_chart.setVisibleXRangeMaximum(100f)
+        bar_chart.data = data
+
+        dataSet.colors = color
+//        data.barWidth = 100f
+
+        bar_chart.highlightValues(null)
+        bar_chart.invalidate()
+
+        bar_chart.axisLeft.axisMinimum = 0f
+        bar_chart.axisRight.axisMinimum = 0f
+
+        bar_chart.axisLeft.setDrawLabels(false)
+        bar_chart.axisRight.setDrawLabels(false)
+        bar_chart.xAxis.setDrawLabels(false)
+        bar_chart.xAxis.isEnabled = false
+        bar_chart.axisLeft.isEnabled = false
+        bar_chart.axisRight.isEnabled = false
+
+        bar_chart.setTouchEnabled(false)
+        bar_chart.description.isEnabled = false
     }
 
     override fun observerViewModel() {
