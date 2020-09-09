1e068cc7ccfde33ddbb52d340d900875538a4cd5
YJH <chuchoun1234@gmail.com>
2020-09-09 09:21:52 +0900

    Update VoteAddAdapter.kt

--- Hackathon/app/src/main/java/com/example/hackathon/adapter/VoteAddAdapter.kt
+++ Hackathon/app/src/main/java/com/example/hackathon/adapter/VoteAddAdapter.kt
@@ -35,7 +35,11 @@ class VoteAddAdapter : RecyclerView.Adapter<VoteAddAdapter.Holder>() {
             if(voteList.voteList.size == 0){
                 inputContent.hint ="내용"
             }else{
+<<<<<<< Updated upstream
                 inputContent.hint = voteList.voteList.toString()
+=======
+                inputContent.hint = voteList.voteList[]
+>>>>>>> Stashed changes
             }
         }
     }
